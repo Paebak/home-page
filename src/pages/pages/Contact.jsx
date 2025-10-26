1e1204b267609c1ac5384e97363ac0bfc0b38278
Matthew_Downs <142461031+Paebak@users.noreply.github.com>
2025-10-26 11:13:49 -0700

    Create Contact.jsx
    Add email later -

---
+++ src/pages/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>Email: ADD@EMAIL.LATER</p>
+      <p>GitHub: <a href="https://github.com/Paebak" target="_blank" rel="noreferrer">Paebak</a></p>
+    </section>
+  );
+}
