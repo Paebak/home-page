beded11549fb27b74e3d3f6554dccdf45204f62e
Max Keneau <gart@gart.sh>
2025-10-26 12:32:17 -0700

    rewrite build script

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // Deployed on root at neofound.org, so base = "/"
 export default defineConfig({
-  plugins: [react()],
-  base: "/"
+	plugins: [react()],
+	root: "src",
+	build: {
+		outDir: "../dist",
+	},
 });
